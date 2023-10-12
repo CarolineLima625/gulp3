f7f9cb864c092d21fd5d0a85df4272248b5d0461
carolinelima <carolinerlima625@gmail.com>
2023-10-11 23:51:39 -0300

    atualizacao

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,47 @@ const concat = require("gulp-concat")
 const cssmin = require("gulp-cssmin")
 const rename = require("gulp-rename")
 const uglify = require("gulp-uglify")
+const image = require("gulp-image")
 
 function tarefasCSS(cb){
-    return gulp.src('./vendor/**/*.css')
+    return gulp.src(
+        './node_modules/bootstrap/dist/css/bootstrap.css', 
+        './vendor/owl/css/owl.css',
+        "./node_modules/@fortawesome/fontawesome-free/css/fontawesome.css",
+        "./vendor/jquery-ui.custom/jquery-ui.css")
         .pipe(concat('libs.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest("./dist/css"))
+
+
 }
 
 function tarefasJS(){
-    return gulp.src("./vendor/**/*.js")
+    return gulp.src(["./vendor/**/*.js"])
         .pipe(concat("libs.js"))
         .pipe(uglify())
         .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest("./dist/js"))
 }
 
+function tarefasImagem(){
+    
+    return gulp.src("./images/*")
+        .pipe(image({
+            pngquant: true,
+            optipng: false,
+            zopflipng: true,
+            jpegRecompress: false,
+            mozjpeg: true,
+            gifsicle: true,
+            svgo: true,
+            concurrent: 10,
+            quiet: true
+        }))
+        .pipe(gulp.dest('./dist/images'))
+}
+
 exports.styles = tarefasCSS
 exports.scripts = tarefasJS
+exports.images = tarefasImagem
